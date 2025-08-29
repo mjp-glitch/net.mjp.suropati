2fcf889b6419ff356fe16802e9b8e35521d2e346
mjp-glitch <wa.ipoelnet@gmail.com>
2025-08-29 22:05:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 {
     "success": true,
     "plugins": [{
+        "app": "https://raw.githubusercontent.com/mjp-glitch/net.mjp.suropati/refs/heads/main/app.js?1",
         "welcome": "https://raw.githubusercontent.com/mjp-glitch/net.mjp.suropati/refs/heads/main/welcome.js?123",
         "router": "https://raw.githubusercontent.com/mjp-glitch/net.mjp.suropati/refs/heads/main/router.js?1"
     }]

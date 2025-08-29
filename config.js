43c91cb25059716d038967255780728c6c110622
mjp-glitch <wa.ipoelnet@gmail.com>
2025-08-29 21:53:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 {
-  "success":true,
-  "plugins":{
-    "welcome":"https://raw.githubusercontent.com/mjp-glitch/net.mjp.suropati/refs/heads/main/welcome.js?1","router":"https://raw.githubusercontent.com/mjp-glitch/net.mjp.suropati/refs/heads/main/router.js?1"
-  }
+    "success": true,
+    "plugins": {
+        "welcome": "https://raw.githubusercontent.com/mjp-glitch/net.mjp.suropati/refs/heads/main/welcome.js?1",
+        "router": "https://raw.githubusercontent.com/mjp-glitch/net.mjp.suropati/refs/heads/main/router.js?1"
+    }
 }
